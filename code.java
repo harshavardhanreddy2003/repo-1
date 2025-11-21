4aee6b8b7a27ffd73681f5ebd9647fcff23a5e01
Harsha <143harshavardhanreddy143@gmail.com>
2025-11-21 20:36:45 +0530

    java

---
+++ code.java
@@ -0,0 +1,13 @@
+public class AdditionExample {
+    public static void main(String[] args) {
+        // Declare and initialize variables
+        int number1 = 5;
+        int number2 = 7;
+
+        // Add the numbers
+        int sum = number1 + number2;
+
+        // Display the result
+        System.out.println("The sum is: " + sum);
+    }
+}
